eacf74cd420343311f7383aa4f7d20576310130e
Walerian Sokolowski <r-logging@wlogsolutions.com>
2019-01-16 17:10:03 +0000

    version 0.9-105

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,9 @@
 ##
-## this is part of the R-logging package. the R-logging package is free
-## software: you can redistribute it and/or modify it under the terms of the
-## GNU General Public License as published by the Free Software Foundation,
-## either version 3 of the License, or (at your option) any later version.
+## this is part of the logging package. the logging package is free
+## software: you can redistribute it as well as modify it under the terms of
+## the GNU General Public License as published by the Free Software
+## Foundation, either version 3 of the License, or (at your option) any later
+## version.
 ##
 ## this program is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -10,52 +11,129 @@
 ## General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with the nens libraray.  If not, see
-## <http://www.gnu.org/licenses/>.
+## along with the nens libraray.  If not, see http://www.gnu.org/licenses/.
 ##
-## Copyright (c) 2009-2013 by Mario Frasca
+## Copyright (c) 2009..2013 by Mario Frasca
 ##
 
-#################################################################################
 
-## sample actions for handlers
+#'
+#' Predefined(sample) handler actions
+#'
+#' When you define a handler, you specify its name and the associated action.
+#' A few predefined actions described below are provided.
+#'
+#' A handler action is a function that accepts a formated message and handler
+#' configuration.
+#'
+#' Messages passed are filtered already regarding loglevel.
+#'
+#' \dots parameters are used by logging system to interact with the action. \dots can
+#' contain \var{dry} key to inform action that it meant to initialize itself. In the case
+#' action should return TRUE if initialization succeded.
+#'
+#' If it's not a dry run \dots contain the whole preformated \var{logging.record}.
+#' A \var{logging.record} is a named list and has following structure:
+#' \describe{
+#'   \item{msg}{contains the real formatted message}
+#'   \item{level}{message level as numeric}
+#'   \item{levelname}{message level name}
+#'   \item{logger}{name of the logger that generated it}
+#'   \item{timestamp}{formatted message timestamp}
+#' }
+#'
+#' @param msg A formated message to handle.
+#' @param handler The handler environment containing its options. You can
+#'   register the same action to handlers with different properties.
+#' @param ... parameters provided by logger system to interact with the action.
+#'
+#' @examples
+#' ## define your own function and register it with a handler.
+#' ## author is planning a sentry client function.  please send
+#' ## any interesting function you may have written!
+#'
+#' @name inbuilt-actions
+NULL
 
-## a handler is a function that accepts a logging.record and a
-## configuration.
-
-## a logging.record contains the real message, its level, the name of the
-## logger that generated it, a timestamp.
-
-## a configuration contains a formatter (a function taking a
-## logging.record and returning a string), a numeric level (only records
-## with level equal or higher than that are taken into account), an
-## action (writing the formatted record to a stream).
-
-writeToConsole <- function(msg, handler, ...)
-{
-  if(length(list(...)) && 'dry' %in% names(list(...)))
+#' @rdname inbuilt-actions
+#'
+#' @details
+#' \code{writeToConsole} detects if crayon package is available and uses it
+#' to color messages. The coloring can be switched off by means of configuring
+#' the handler with \var{color_output} option set to FALSE.
+#'
+#' @export
+#'
+writeToConsole <- function(msg, handler, ...) {
+  if (length(list(...)) && "dry" %in% names(list(...))) {
+    if (!is.null(handler$color_output) && handler$color_output == FALSE) {
+      handler$color_msg <- function(msg, level_name) msg
+    } else {
+      handler$color_msg <- .build_msg_coloring()
+    }
     return(TRUE)
-  cat(paste(msg, '\n', sep=''))
+  }
+
+  stopifnot(length(list(...)) > 0)
+
+  level_name <- list(...)[[1]]$levelname
+  msg <- handler$color_msg(msg, level_name)
+  cat(paste0(msg, "\n"))
 }
 
-writeToFile <- function(msg, handler, ...)
-{
-  if(length(list(...)) && 'dry' %in% names(list(...)))
-    return(exists('file', envir=handler))
-  cat(paste(msg, '\n', sep=''), file=with(handler, file), append=TRUE)
+.build_msg_coloring <- function() {
+  crayon_env <- tryCatch(asNamespace("crayon"),
+                         error = function(e) NULL)
+
+  default_color_msg <- function(msg, level_name) msg
+  if (is.null(crayon_env)) {
+    return(default_color_msg)
+  }
+
+  if (is.null(crayon_env$make_style) ||
+      is.null(crayon_env$combine_styles) ||
+      is.null(crayon_env$reset)) {
+    return(default_color_msg)
+  }
+
+  color_msg <- function(msg, level_name) {
+    style <- switch(level_name,
+                    "FINEST" = crayon_env$make_style("gray80"),
+                    "FINER" = crayon_env$make_style("gray60"),
+                    "FINE" = crayon_env$make_style("gray60"),
+                    "DEBUG" = crayon_env$make_style("deepskyblue4"),
+                    "INFO" = crayon_env$reset,
+                    "WARNING" = crayon_env$make_style("darkorange"),
+                    "ERROR" = crayon_env$make_style("red4"),
+                    "CRITICAL" =
+                      crayon_env$combine_styles(crayon_env$bold,
+                                                crayon_env$make_style("red1")),
+                    crayon_env$make_style("gray100"))
+    res <- paste0(style(msg), crayon_env$reset(""))
+    return(res)
+  }
+  return(color_msg)
 }
 
-#################################################################################
+
+#' @rdname inbuilt-actions
+#'
+#' @details \code{writeToFile} action expects file path to write to under
+#'  \var{file} key in handler options.
+#'
+#' @export
+#'
+writeToFile <- function(msg, handler, ...) {
+  if (length(list(...)) && "dry" %in% names(list(...)))
+    return(exists("file", envir = handler))
+  cat(paste0(msg, "\n"), file = with(handler, file), append = TRUE)
+}
 
 ## the single predefined formatter
-
 defaultFormat <- function(record) {
   ## strip leading and trailing whitespace from the final message.
-  msg <- sub("[[:space:]]+$", '', record$msg)
-  msg <- sub("^[[:space:]]+", '', msg)
-  text <- paste(record$timestamp, paste(record$levelname, record$logger, msg, sep=':'))
+  msg <- trimws(record$msg)
+  text <- paste(record$timestamp,
+                paste(record$levelname, record$logger, msg, sep = ":"))
+  return(text)
 }
-
-#################################################################################
-
-#################################################################################
